3e2f3ff70c3dbd4b746b8c9805e148a496082adc
Anton <antonoliverkiiski@gmail.com>
2023-01-18 12:22:44 +0200

    Add ngIf directive and randomizastion to server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -13,7 +13,9 @@ export class ServerComponent implements OnInit {
     return this.serverStatus;
   }
 
-  constructor() {}
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit(): void {}
 }
