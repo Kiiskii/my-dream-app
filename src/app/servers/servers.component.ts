dd21e6368c991834c2264f0c828aefd31fb123d4
Anton <antonoliverkiiski@gmail.com>
2023-01-18 12:39:10 +0200

    Add ngFor directive to add more serves to the list by pressing the "Add server" button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Tertserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Server name is ' + this.serverName;
   }
